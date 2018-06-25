b745961b689253bc9a0df2ead4c4dbec31a92b92
maayankfir <mayankfir@gmail.com>
2018-06-25 16:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const app = "I don't do much."
 
-var dodger = document.grtElementById ('dodger') 
+var dodger = document.grtElementById ('dodger')
 
-document.addEventListener ('keydown , function (e)') {
-  if (e.which === 39) {
-    var rightNumbers = dodger.style.right.replace ('px', '') 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px','');
+  var left = parseInt(leftNumbers, 10);
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    var rightNumbers = dodger.style.right.replace ('px', '')
     var right = parseInt (rightNumbers, 10)
-    if (right > 0) {
+    if (right < 360) {
       dodger.style.right = '${right -1}px'
     }
   }
