c17d790da0fd585299c94eeac560c8b6550187e1
maayankfir <mayankfir@gmail.com>
2018-06-25 16:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+var dodger = document.grtElementById ('dodger') 
+
+document.addEventListener ('keydown , function (e)') {
+  if (e.which === 39) {
+    var rightNumbers = dodger.style.right.replace ('px', '') 
+    var right = parseInt (rightNumbers, 10)
+    if (right > 0) {
+      dodger.style.right = '${right -1}px'
+    }
+  }
+}
+document.addEventListener ('keydown , function (e)') {
+  if (e.which === 39) {
+    moveDodgeRight ()
+  }
+  
