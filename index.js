28b4a28f0e60f1e99fc46acce97b809efb8bfc9a
maayankfir <mayankfir@gmail.com>
2018-06-25 17:08:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function moveDodgerLeft() {
     }
   }
 
-
 function moveDodgerRight() {
     var rightNumbers = dodger.style.right.replace('px', '')
     var right = parseInt (rightNumbers, 10)
@@ -20,7 +19,6 @@ function moveDodgerRight() {
       dodger.style.right = '${right -1}px'
     }
   }
-
 
 document.addEventListener('keydown', function(e) {
 if (e.which === 37) {
