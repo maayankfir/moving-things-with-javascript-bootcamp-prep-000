5cc4aa721150275896a102943f3ad329f9715c19
maayankfir <mayankfir@gmail.com>
2018-06-25 16:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var dodger = document.grtElementById ('dodger')
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px','')
   var left = parseInt(leftNumbers, 10)
-  
+
   if(left > 0) {
     dodger.style.left = `${left - 1}px`
     }
@@ -15,12 +15,12 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
     var rightNumbers = dodger.style.right.replace ('px', '')
     var right = parseInt (rightNumbers, 10)
-    
+
     if (right < 360) {
       dodger.style.right = '${right -1}px'
     }
   }
-}
+
 
 document.addEventListener('keydown', function(e) {
 if (e.which === 37) {
